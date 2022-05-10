cbc67dd11d7ffd785fbe9a0b84d88fc6658df1ae
Vincent Boucheny <vboucheny@squareup.com>
2022-05-10 16:36:42 +1200

    Create init.el file with magit capability

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; init.el --- Load the full configuration
+
+;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
+;;(setq debug-on-error t)
+
+
+
+(setq mac-command-modifier 'meta)
+
+;; elpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; magit
+(package-install 'magit)
